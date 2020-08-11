82fc21caf1d5ece6920a1c8d9c31ff2da3a62168
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2020-08-11 14:01:30 +0200

    Updates

---
+++ precache-manifest.4c8b868e6ce8a97f047a67eba691a752.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0609fe2c7d0a57eec17d65ea8570332",
+    "url": "/.github.io/todomake/index.html"
+  },
+  {
+    "revision": "54e7682979a934462a32",
+    "url": "/.github.io/todomake/static/css/main.654cecd4.chunk.css"
+  },
+  {
+    "revision": "2b7102446546307994dc",
+    "url": "/.github.io/todomake/static/js/2.c881ae52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/.github.io/todomake/static/js/2.c881ae52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54e7682979a934462a32",
+    "url": "/.github.io/todomake/static/js/main.3fff53ff.chunk.js"
+  },
+  {
+    "revision": "8ca0f63f90bc31cbfee4",
+    "url": "/.github.io/todomake/static/js/runtime-main.379d8d3d.js"
+  }
+]);
