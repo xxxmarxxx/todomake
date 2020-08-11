406fdaf3c2f46d098dfcef4f28c91a2afdc91ea2
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2020-08-11 16:23:49 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -3,16 +3,16 @@ import TodoContainer from "./components/ToDoContainer";
 import "./App.scss";
 
 function App() {
+  return (
+    <div className="App">
+      <p
+        style={{ textAlign: "center", marginBottom: "20px", background: "red" }}
+      >
+        Hier kommt unsere ToDo hin (this text is from App.js). test now
+      </p>
+      <TodoContainer text="Mein text kommt als property" />
+    </div>
+  );
+}
 
-     return (
-       <div className="App">
-        <p style={{textAlign:"center", marginBottom: "20px", background: "red"}}>
-          Hier kommt unsere ToDo hin (this text is from App.js). 
-          </p>
-        <TodoContainer text="Mein text kommt als property 2"/>
-        
-       </div>
-       );
-     }
- 
 export default App;
